76f49e4bd94794901df61814a92cec98fee90763
Laureano <laureanosciacaluga@gmail.com>
2023-12-18 21:30:54 -0300

    Se agrego pagina reloj digital

--- src/components/ProjectComponent.jsx
+++ src/components/ProjectComponent.jsx
@@ -6,15 +6,27 @@ function ProjectComponent ({ pjName, pjTech, pjImg, pjText }) {
 
     return(
         <div className="pjCard">
-            <h2 className="pjName">{pjName}</h2>
-            <div className="pjTags">
-                {
-                    pjTech.map((tech, index) => (
-                        <h2 key={index} className='tech'>
-                            <ProjectTag techName={tech} />
-                        </h2>
-                    ))
-                }
+            <div className="img">
+                <img src={ pjImg } alt="" />
+            </div>
+            <div className="info">
+                <h2 className="pjName">
+                    <a href="https://digitalclock-lau.netlify.app/" target='_blank' rel="noreferrer">
+                        {pjName}
+                    </a>
+                </h2>
+                <p className="description">
+                    {pjText}
+                </p>
+                <div className="tags">
+                    {
+                        pjTech.map((tech, index) => (
+                            <h2 key={index} className='tech'>
+                                <ProjectTag techName={tech} />
+                            </h2>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,14 +1,32 @@
 import ProjectComponent from '../components/ProjectComponent'
+import "./projects.css"
 
 function Projects () {
     return(
         <div className="projects" id="projects">
             <h1>Projects</h1>
             <div className="projectsList">
-                <ProjectComponent pjName={'Digital clock'} pjTech={['JS', 'HTML', 'CSS']}/>
-                <ProjectComponent pjName={'Tic tac toe'} pjTech={['JS', 'REACT', 'HTML', 'CSS']}/>
-                <ProjectComponent pjName={'Hackathon page'} pjTech={['JS', 'HTML', 'CSS']}/>
-                <ProjectComponent pjName={'Portfolio'} pjTech={['JS', 'REACT', 'HTML', 'CSS']}/>
+                <ProjectComponent 
+                    pjName={'Digital clock'} 
+                    pjTech={['JS', 'HTML', 'CSS']}
+                    pjImg={'/src/assets/digital-clock.png'}
+                    pjText={'Este proyecto web simple ofrece la hora y fecha actuales de forma clara y accesible. Con un diseño minimalista, proporciona una experiencia intuitiva para los usuarios que buscan información temporal rápida. '}
+                />
+                
+                <ProjectComponent 
+                    pjName={'Tic tac toe'} 
+                    pjTech={['JS', 'REACT', 'HTML', 'CSS']}
+                />
+                
+                <ProjectComponent
+                    pjName={'Hackathon page'}
+                    pjTech={['JS', 'HTML', 'CSS']}
+                />
+
+                <ProjectComponent
+                    pjName={'Portfolio'} 
+                    pjTech={['JS', 'REACT', 'HTML', 'CSS']}
+                />
             </div>
         </div>
     )
